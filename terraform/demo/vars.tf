f2e5c82e951f4d258cd2664f6d5233f3e9da9f1d
Ivan Titovich <i.a.titovich@gmail.com>
2022-08-26 21:40:24 +0300

    Terraform cloud registration

---
+++ terraform/demo/vars.tf
@@ -0,0 +1,79 @@
+variable "yc_token" {
+   default = ""
+}
+
+variable "yc_cloud_id" {
+  default = ""
+}
+
+variable "yc_folder_id" {
+  default = ""
+}
+
+variable "yc_region" {
+  default = ""
+}
+
+
+
+locals {
+  news_names = {
+    default = "stage"
+    stage = "stage"
+    prod = "prod"
+  }
+  news_cores = {
+    default = 2
+    stage = 2
+    prod = 2
+  }
+  news_platforms = {
+    default = "standard-v1"
+    stage = "standard-v1"
+    prod = "standard-v2"
+  }
+  news_disk_size = {
+    default = 20
+    stage = 20
+    prod = 40
+  }
+  news_instance_count = {
+    default = 1
+    stage = 1
+    prod = 2
+  }
+
+  vpc_subnets = {
+    default = [
+      {
+        "v4_cidr_blocks": [
+          "10.128.0.0/24"
+        ],
+        "zone": var.yc_region
+      }
+    ]
+    stage = [
+      {
+        "v4_cidr_blocks": [
+          "10.128.0.0/24"
+        ],
+        "zone": var.yc_region
+      }
+    ]
+    prod = [
+      {
+        zone           = "ru-central1-a"
+        v4_cidr_blocks = ["10.128.0.0/24"]
+      },
+      {
+        zone           = "ru-central1-b"
+        v4_cidr_blocks = ["10.129.0.0/24"]
+      },
+      {
+        zone           = "ru-central1-c"
+        v4_cidr_blocks = ["10.130.0.0/24"]
+      }
+    ]
+  }
+}
+
